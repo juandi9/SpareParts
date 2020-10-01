b21a9ba8328ad72ef25acc008bd1a62df3c7edc1
juandi9 <64390772+juandi9@users.noreply.github.com>
2020-09-30 20:18:12 -0500

    agregue el modelo existenten

---
+++ conexion/Conect.php
@@ -0,0 +1,9 @@
+<?php
+
+class Conect {
+    public static function conectar(){
+        $pdo = new PDO('mysql:host=localhost;dbname=dbspare;charset=utf8','root','');
+        $pdo-> setAttribute(PDO ::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+}
